4b978b84fbded3afe642682d68b00461ce9cf5b5
uu64 <yusuke.t.0808@gmail.com>
2020-11-24 00:13:43 +0900

    fix: problem 22

--- problem022.py
+++ problem022.py
@@ -1,4 +1,11 @@
+import string
+
 path = "./names.txt"
+
+alphabets = string.ascii_lowercase
+score_dict = {}
+for i in range(len(alphabets)):
+    score_dict[alphabets[i]] = i + 1
 
 def delte_double_quote(s):
     return s.strip('"')
@@ -8,5 +15,15 @@ data = f.read().split(",")
 f.close()
 
 data = map(delte_double_quote, data)
+data.sort()
 
-print(data)
+ans = 0
+for i in range(len(data)):
+    score = 0
+    s = data[i].lower()
+    for c in s:
+        score = score + score_dict[c]
+    score = score * (i + 1)
+
+    ans = ans + score
+print(ans)
