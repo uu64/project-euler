69f4d355f1462689d124649b940ca2d118815325
uu64 <yusuke.t.0808@gmail.com>
2020-12-02 23:02:22 +0900

    add: problem 024

---
+++ problem024.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+DIGITS = 10
+INDEX = 1000000
+
+counter = 0
+def f(s, num_set):
+    global counter
+    if len(num_set) == 0:
+        counter += 1
+        if counter == INDEX:
+            print(s)
+
+    for i in num_set:
+        c = str(i)
+        if not c in s:
+            tmp = num_set[:]
+            tmp.remove(c)
+            f(s+c, tmp)
+        if counter == INDEX:
+            return
+
+f('', [ str(i) for i in range(DIGITS) ])
