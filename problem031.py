dcc446d945a6aac791cb9c191792b977fcd65a09
uu64 <yusuke.t.0808@gmail.com>
2021-07-21 00:58:53 +0900

    update: problem 32 using memo

--- problem031.py
+++ problem031.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
-def rec(s, coins):
-    if s == 200:
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+amount = 200
+memo = {}
+
+def rec(cur, coins):
+    global memo
+    if cur == amount:
         return 1
 
-    if s < 200:
+    if cur < amount:
         count = 0
         for i, v in enumerate(coins):
-            count += rec(s + v, coins[i:])
+            new = cur + v
+            if new > 200:
+                continue
+            if (v, new) not in memo:
+                res = rec(new, coins[i:])
+                memo[(v, new)] = res
+            count += memo[(v, new)]
         return count
 
     return 0
 
-print(rec(0, [1, 2, 5, 10, 20, 50, 100, 200]))
+print(rec(0, coins))
 
